74378de9f6a6ffc25241a2c5d53badb75bdbb028
smallkirby <ssmallkirby@gmail.com>
2023-08-12 16:52:58 +0900

    about: init likes boilerplate
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,22 @@
 import Image from 'next/image';
 import NormalLink from '@/components/common/NormalLink';
 
+function LikeCard({ title, img }: { title: string; img: string }) {
+  return (
+    <div
+      className="card card-compact m-2 h-52 w-full cursor-pointer bg-gv-bg0h
+            shadow-xl  duration-300 hover:bg-gv-bg0 md:w-64"
+    >
+      <figure className="h-full w-full">
+        <Image src={img} alt={title} width={400} height={500} />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{title}</h2>
+      </div>
+    </div>
+  );
+}
+
 export default function About() {
   return (
     <div>
@@ -64,6 +80,20 @@ export default function About() {
               </li>
             </ul>
           </div>
+        </div>
+      </div>
+
+      <div className="mt-8 flex flex-col">
+        <div className="mb-1 border-b-[1px] border-gv-bg2 pb-4">
+          <h2 className="text-2xl font-bold text-gv-aqua2">Likes</h2>
+        </div>
+        <div className="mr-4 flex flex-col md:flex-row md:flex-wrap">
+          <LikeCard title="Keyboards" img="/about/corne.jpg" />
+          <LikeCard title="Game" img="/about/controller.jpg" />
+          <LikeCard title="Dog" img="/about/dog.jpg" />
+          <LikeCard title="Editor" img="/about/vscode-sc.png" />
+          <LikeCard title="Color Scheme" img="/about/color-scheme.png" />
+          <LikeCard title="OS" img="/about/ubuntu.jpg" />
         </div>
       </div>
     </div>
