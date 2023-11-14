13681024fce04f34eb10085ad8f08ad8bdf4f0ad
smallkirby <ssmallkirby@gmail.com>
2023-11-14 14:18:13 +0900

    change menu style depending on platform
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -3,6 +3,7 @@
 import { Link, MenuBook, Info } from '@mui/icons-material';
 import HomeIcon from '@mui/icons-material/Home';
 import Breadcrumb from '@/components/nav/Breadcrumb';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function IconButton(props: { icon: React.ReactNode; link: string }) {
   return (
@@ -16,17 +17,52 @@ function IconButton(props: { icon: React.ReactNode; link: string }) {
 
 export default function Header() {
   return (
-    <header>
-      <div className="navbar w-full justify-between bg-gv-bg0 px-4">
+    <header className="navbar">
+      <div className="w-full justify-between bg-gv-bg0 px-4">
         <div>
           <Breadcrumb />
         </div>
-        <div className="flex w-full justify-end">
-          <IconButton icon={<HomeIcon />} link="/" />
-          <IconButton icon={<Info />} link="/about" />
-          <IconButton icon={<MenuBook />} link="/activity" />
-          <IconButton icon={<Link />} link="/link" />
-        </div>
+      </div>
+
+      <div className="block flex-none md:hidden">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <details>
+              <summary>
+                <MenuIcon />
+              </summary>
+              <ul className="right-1 z-40 !mt-0 rounded-sm bg-gv-bg0h pt-0 text-left">
+                <li>
+                  <a href="/" className="underline">
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a href="/about" className="underline">
+                    About
+                  </a>
+                </li>
+                <li>
+                  <a href="/activity" className="underline">
+                    Activity
+                  </a>
+                </li>
+                <li>
+                  <a href="/link" className="underline">
+                    Links
+                  </a>
+                </li>
+              </ul>
+            </details>
+          </li>
+        </ul>
+      </div>
+
+      <div className="hidden w-full justify-end md:flex">
+        <IconButton icon={<HomeIcon />} link="/" />
+        <IconButton icon={<Info />} link="/about" />
+        <IconButton icon={<MenuBook />} link="/activity" />
+        <IconButton icon={<Link />} link="/link" />
       </div>
     </header>
   );
