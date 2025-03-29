74f9c69e532d3b65437f8fe6bba6148074cebacc
smallkirby <ssmallkirby@gmail.com>
2025-03-29 20:22:56 +0900

    test: allow 20000 pixels diff
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- tests/screenshot.test.ts
+++ tests/screenshot.test.ts
@@ -1,22 +1,24 @@
 import { expect, test } from '@playwright/test';
 
+const maxDiffPixels = 20000;
+
 test('index', async ({ page }) => {
   await page.goto('http://localhost:3000');
   await expect(page).toHaveScreenshot('index.png', {
-    maxDiffPixels: 1000,
+    maxDiffPixels,
   });
 });
 
 test('about', async ({ page }) => {
   await page.goto('http://localhost:3000/about');
   await expect(page).toHaveScreenshot('about.png', {
-    maxDiffPixels: 1000,
+    maxDiffPixels,
   });
 });
 
 test('activity', async ({ page }) => {
   await page.goto('http://localhost:3000/activity');
   await expect(page).toHaveScreenshot('activity.png', {
-    maxDiffPixels: 1000,
+    maxDiffPixels,
   });
 });
